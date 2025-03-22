74ebaa0ec55cfaaddabb318a03ad486074cb4ed9
Joykinuthia <joyrosekinuthia@gmail.com>
2025-03-23 00:39:18 +0300

    display character details

--- src/index.js
+++ src/index.js
@@ -105,6 +105,8 @@ document.addEventListener("DOMContentLoaded",() =>{
         displayCharacterDetails(data));
         characterBar.appendChild(span);
 
+        displayCharacter(data);
+
     })
     
     
