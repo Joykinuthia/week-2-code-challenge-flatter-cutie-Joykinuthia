ebbb1a60b69e3ed9269eaad273cdd903f625cace
Joykinuthia <joyrosekinuthia@gmail.com>
2025-03-22 17:24:12 +0300

    initial commit - coming up with the functions

--- src/index.js
+++ src/index.js
@@ -1,1 +1,22 @@
 // Your code here
+document.addEventListener("DOMContentLoaded",() =>{
+    const characterBar = document.getElementById("character-bar");
+    const characterForm = document.getElementById("character-form");
+    const detailedInfo = document.getElementById("detailed-info");
+    const imageElement = document.getElementById("image");
+    const nameElement = detailedInfo.querySelector("p");
+    const nameInput = document.getElementById("name");
+    const votesInput = document.getElementById("votes");
+    const votesForm = document.getElementById("votes-form");
+    const voteCount = document.getElementById("vote-count");
+    const characterName = document.getElementById("name");
+    const imageUrlInput = document.getElementById("image-url");
+    const resetButton = document.getElementById("reset-btn");
+})
+    
+   
+    
+    
+    
+    
+    
